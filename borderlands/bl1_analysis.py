e38a7075bc07a01f5c6130e77fdb44b7b3e4ce4e
hbrown10 <haydenbrown@comcast.net>
2022-03-19 23:32:07 -0400

    cleaned CSV output, added LootCategory filtering column

--- borderlands/bl1_analysis.py
+++ borderlands/bl1_analysis.py
@@ -2,8 +2,28 @@
 
 import pandas as pd
 import xml.etree.ElementTree as ET
+from itertools import product
 
-def parse_xml(path):
+pd.options.mode.chained_assignment = None  # default='warn'
+
+uniques = {
+    'combat_rifles':
+        ['Sentinel','Raven','Guardian','Destroyer','Avenger'],
+    'machine_guns':
+        ['Grinder','Ajax','Chopper','Draco','Ogre','Bastard','Revolution','Serpens'],
+    'repeaters':
+        ['Finger','Dove','Krom','Chiquito','Knoxx','Athena','Troll','Hornet','Invader',
+         'Firehawk','Gemini','Protector','Violator','Rebel','Nemesis'],
+    'machine_pistols':
+        ['Clipper','Reaper','Thanatos','Vengeance','Stalker'],
+    'revolvers':
+        ['Patton','Madjack','Chimera','Anaconda','Unforgiven','Defiler','Equalizer','Aries'],
+    'submachine_guns':
+        ['Shredder','Spy','Typhoon','Wildcat','Bitch','Hellfire','Savior','Gasher','Tsunami']
+}
+
+
+def parse_xml(path, incl_knoxx=True):
     # first, we parse the XML file into an ElementTree for QOL
     tree = ET.parse(path)
     parts = {} # dict to store essential part info
@@ -51,11 +71,32 @@ def parse_xml(path):
                 parts[name][attr.tag] = attr.text
 
     df = pd.DataFrame.from_dict(parts, 'index')
-    df2 = df[~df['PartType'].isin(['Gear Type', 'Item Grade', 'Manufacturer', 'Bullet'])]
+    df2 = df.loc[~df['PartType'].isin(['Gear Type', 'Item Grade', 'Manufacturer', 'Bullet'])]
+
+    weap_type = []
+    part_names = []
+
+    for rowname in list(df2.index):
+        split_name = rowname.split('.')
+        weap_type.append(split_name[0].replace('gd_weap_', ''))
+        part_names.append(split_name[2])
+
+    df2['LootCategory'] = weap_type
+    df2.index = part_names
+
+    cols = list(df2)
+    cols.insert(0, cols.pop(cols.index('LootCategory')))
+    df2 = df2.loc[:, cols]
+
+    if not incl_knoxx:
+        df2 = df2.loc[~df2['LootCategory'].str.contains("UniqueParts", case=False)]
+
     filename = path.replace('.xml','').replace('xml/', 'csv/')
+    if incl_knoxx: filename += "_incl_knoxx"
     filename += ".csv"
 
     df2.to_csv(filename)
+
     return df2
 
 
@@ -63,13 +104,25 @@ def generate(df):
 
     # collect a list of unique PartType values
     part_types = df['PartType'].unique()
-    print(part_types)
+    type_dicts = {}
+
+    for part_type in part_types:
+        filtered_df = df[(df['PartType'] == part_type)]
+        part_type_list = []
+        for index, row in filtered_df.iterrows():
+            part_type_list.append(index)
+        type_dicts[part_type] = part_type_list
+
+
+
+    all_permutations = [dict(zip(type_dicts, x)) for x in product(*type_dicts.values())]
+    print(len(all_permutations))
 
     return 0
 
 if __name__ == "__main__":
-    g_df = parse_xml('xml/WeaponParts.xml')
-    s_df = parse_xml('xml/ShieldParts.xml')
+    g_df = parse_xml('xml/WeaponParts.xml', incl_knoxx=False)
+    s_df = parse_xml('xml/ShieldParts.xml', incl_knoxx=False)
 
-    guns = generate(g_df)
-    shields = generate(s_df)
+    #guns = generate(g_df)
+    #shields = generate(s_df)
